aad615877d7c543b274ee95b350a3bb4cc356e07
João Vitor <142240131+Gomesdev09@users.noreply.github.com>
2023-09-26 10:41:57 -0300

    colors

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 
 const TodoForm = () => {
+  const [value, setInterval(() => {
+    
+  }, interval);]
   return (
     <div className="todo-form">
        <h2>Criar tarefa</h2>
        <form>
         <input type='text' placeholder="Digite o titulo"/>
         <select>
-            <option value=" "> Selecione uma categoria</option>
+            <option value=""> Selecione uma categoria</option>
             <option value="Trabalho">Trabalho</option>
             <option value="Pessoal">Pessoal</option>
             <option value="Estudos">Estudos</option>
